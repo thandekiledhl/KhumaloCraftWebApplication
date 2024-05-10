b05ede3a449f4efbfeb1a2c19b10d45dc69f2212
thandekiledhl <127399456+thandekiledhl@users.noreply.github.com>
2024-05-10 20:33:32 +0200

    Add files via upload
    Added files are KhumaloCraftPart2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using DatabaseLayer.ApplicationContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDBContext>(o => o.UseSqlServer("Data Source=THANDEKILE\\SQLEXPRESS;Initial Catalog=KhumaloCraft;Integrated Security=True;Trust Server Certificate=True"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
